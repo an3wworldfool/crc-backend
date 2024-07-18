024c3ecccf96ef1c87fd7b8614d78fcf0b038f68
an3wworldfool <ing.germanspdf@gmail.com>
2024-07-17 19:26:01 -0700

    Added CORS headers to lambda

--- update_viewers/app.py
+++ update_viewers/app.py
@@ -41,7 +41,7 @@ def lambda_handler(event, context):
     return {
         'statusCode': 200,
         'headers': {
-            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Headers": 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "OPTIONS,GET"
         },
